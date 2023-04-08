a5bf980fe7273bd5a34ce01c61fb1ac9a6fa8783
Adam Meily <meily.adam@gmail.com>
2023-04-08 11:23:38 -0400

    export DictProxy as public API

--- cincoconfig/fields/__init__.py
+++ cincoconfig/fields/__init__.py
@@ -9,7 +9,7 @@ Cinco Config Fields.
 '''
 from .bool_field import BoolField, FeatureFlagField
 from .bytes_field import BytesField
-from .dict_field import DictField
+from .dict_field import DictField, DictProxy
 from .file_field import FilenameField
 from .include_field import IncludeField
 from .instance_method_field import InstanceMethodField, instance_method
@@ -26,4 +26,4 @@ __all__ = ('StringField', 'IntField', 'FloatField', 'PortField', 'IPv4AddressFie
            'HostnameField', 'DictField', 'ListProxy', 'VirtualField', 'ApplicationModeField',
            'LogLevelField', 'NumberField', 'ChallengeField', 'DigestValue', 'SecureField',
            'BytesField', 'IncludeField', 'InstanceMethodField', 'instance_method',
-           'FeatureFlagField')
+           'FeatureFlagField', 'DictProxy')
